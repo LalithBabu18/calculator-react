1f2f661f5e8a703be85b1792813f30023640c4ee
Lalith <lalithbabumadasi@gmail.com>
2019-09-05 16:05:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  state = {
+    count : '0' 
+  }
+
+  Handle=(digit)=>{
+    const {count}=this.state
+    this.setState({
+      count:count==='0' ?String(digit):count+digit
+    })
+  }
+  reset=()=>{
+    const {count}=this.state
+    this.setState({
+      count: '0'
+    })
+  }
+    add=()=>{
+      const {count}=this.state
+      if(count.indexof('.')===-1){
+    this.setState({
+          count:count+'.'
+        
+    })
+        }
+  }
+
+  cal=()=>{
+    const {count}=this.state
+    this.setState({
+      count:eval(count)
+    })
+
+  };
+  render(){
+    return (
+      <div>
+        <input value={this.state.count}/>
+        <button onClick={()=>this.Handle(1)}>1</button>
+        <button onClick={()=>this.Handle(2)}>2</button>
+        <button onClick={()=>this.Handle(3)}>3</button>
+        <button onClick={()=>this.Handle(4)}>4</button>
+        <button onClick={()=>this.Handle(5)}>5</button>
+        <button onClick={()=>this.Handle(6)}>6</button>
+        <button onClick={()=>this.Handle(7)}>7</button>
+        <button onClick={()=>this.Handle(8)}>8</button>
+        <button onClick={()=>this.Handle(9)}>9</button>
+        <button onClick={()=>this.Handle(0)}>0</button>
+        <button onClick={()=>this.Handle('+')}>+</button>
+        <button onClick={()=>this.Handle('-')}>-</button>
+        <button onClick={()=>this.Handle('*')}>*</button>
+        <button onClick={()=>this.Handle('/')}>/</button>
+        <button onClick={()=>this.reset()}>c</button>
+        <button onClick={()=>this.addDot()}>.</button>
+        <button onClick={()=>this.cal()}>=</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+
